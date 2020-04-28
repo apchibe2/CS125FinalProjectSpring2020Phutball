03003478a79c397eb18e20ef6454a8a5a3b661f1
woken3 <woken3@illinois.edu>
2020-04-27 20:23:13 -0500

    Merge remote-tracking branch 'remotes/origin/master' added comment

--- app/src/main/java/com/example/cs125finalprojectspring2020phutball/Game.java
+++ app/src/main/java/com/example/cs125finalprojectspring2020phutball/Game.java
@@ -1,5 +1,6 @@
 package com.example.cs125finalprojectspring2020phutball;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,12 +39,15 @@ public class Game extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        leaveGameButton();
+    }
+
+    private void leaveGameButton() {
         Button leaveGame = findViewById(R.id.leave_game_button);
         leaveGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(Game.this, MainActivity.class));
             }
         });
     }
