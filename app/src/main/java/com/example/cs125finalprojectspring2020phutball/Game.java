aeb68306603b8a9d420a3aa2c7c46d217f524ae5
apchibe2 <apchibe2@illinois.edu>
2020-05-04 20:04:02 -0500

    uploading progress

--- app/src/main/java/com/example/cs125finalprojectspring2020phutball/Game.java
+++ app/src/main/java/com/example/cs125finalprojectspring2020phutball/Game.java
@@ -2,28 +2,36 @@ package com.example.cs125finalprojectspring2020phutball;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.cs125finalprojectspring2020phutball.gamelogic.Tile;
 import com.google.android.material.snackbar.Snackbar;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Game extends AppCompatActivity {
+    private FirebaseDatabase database;
 
+    private static int playerCount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference players = database.getReference("players");
         setContentView(R.layout.activity_game);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        players.child("phutball" + playerCount).setValue(new Tile(8,10, 0));
 
         TextView gameState = findViewById(R.id.gamestate_textview);
         ImageView imageView = findViewById(R.id.game_board_imageview);
@@ -39,6 +47,8 @@ public class Game extends AppCompatActivity {
         addPlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                players.child("player" + playerCount).setValue(new Tile(0,0, playerCount));
+                playerCount++;
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -51,7 +61,34 @@ public class Game extends AppCompatActivity {
             }
         });
 
-        // Access a Cloud Firestore instance from your Activity
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //TODO
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle("Ent new player coordinates");
+        // I'm using fragment here so I'm using getView() to provide ViewGroup
+        // but you can provide here any other instance of ViewGroup from your Fragment / Activity
+        View viewInflated = LayoutInflater.from(getApplicationContext()).inflate(findViewById(_____________), false);
+        // Set up the input
+        final EditText xInput = (EditText) viewInflated.findViewById(R.id.xInput);
+        final EditText yInput = (EditText) viewInflated.findViewById(R.id.yInput);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        builder.setView(viewInflated);
+
+        // Set up the buttons
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
